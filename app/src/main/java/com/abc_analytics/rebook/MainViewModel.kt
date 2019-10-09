08ad21ffe9fd4d772fefded2d0a190350aa4adf7
kimiyuki shirai <kimiyuki@gmail.com>
2019-10-09 19:07:52 +0900

    experiment viewmodel

--- app/src/main/java/com/abc_analytics/rebook/MainViewModel.kt
+++ app/src/main/java/com/abc_analytics/rebook/MainViewModel.kt
@@ -3,11 +3,16 @@ package com.abc_analytics.rebook
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import java.util.*
 
 class MainViewModel(private val context: Application) : AndroidViewModel(context) {
   var text = MutableLiveData<String>()
 
   init {
     text.value = ""
+  }
+
+  fun setMyText(){
+    text.value = Date().toString()
   }
 }
--- app/src/main/java/com/abc_analytics/rebook/ui/home/HomeFragment.kt
+++ app/src/main/java/com/abc_analytics/rebook/ui/home/HomeFragment.kt
@@ -8,24 +8,26 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.abc_analytics.rebook.MainViewModel
 import com.abc_analytics.rebook.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+  lateinit var viewModel: MainViewModel
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+    viewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
+    viewModel.text.observe(this, Observer {
+      text_home.text = it
+    })
+    return inflater.inflate(R.layout.fragment_home, container, false)
+  }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    button_home_frag.setOnClickListener {
+      viewModel.setMyText()
     }
+  }
 }
