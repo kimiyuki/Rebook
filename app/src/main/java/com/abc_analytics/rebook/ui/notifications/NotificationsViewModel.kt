8b87c635b5d8cdcc4e6b1b45d20f4f082f724800
kimiyuki shirai <kimiyuki@gmail.com>
2019-10-09 16:06:00 +0900

    setup

---
+++ app/src/main/java/com/abc_analytics/rebook/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.abc_analytics.rebook.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
